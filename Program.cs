54cb14c2882c6c4aef9088b6e3366481c0b75971
Edmond Tu <edmond8tu@berkeley.edu>
2021-12-30 00:17:59 -0800

    need to implement error handling for user input

--- Program.cs
+++ Program.cs
@@ -7,59 +7,71 @@ namespace JanID
     {
         static void Main(string[] args) 
         {
+
             welcomeMsg();
+
+            bool flag = true;
+
+            while (flag) {
+
+                
+                string fileName = "data.json";
+                Food food = getJSON(fileName);
+                
+                //Console.WriteLine($"Meals: {food.Meals[0]}");
+                //Console.WriteLine()
+
             
-            string fileName = "data.json";
-            Food food = getJSON(fileName);
-            
-            //Console.WriteLine($"Meals: {food.Meals[0]}");
-            //Console.WriteLine()
+                //Console.WriteLine($"{Environment.NewLine}Would you like a meal or ")
 
-            Console.WriteLine($"{Environment.NewLine}Press any key to begin!");
-            Console.ReadKey(true);
+                string[] options = {"Meal", "Food"};
+                string mealOrFood = promptOptions("Would you like a meal or a specific food?", options);
 
-            //Console.WriteLine($"{Environment.NewLine}Would you like a meal or ")
+                if (mealOrFood == "Meal") {
+                    Console.WriteLine($"{Environment.NewLine}{randomItem(food.Meals)}");
+                } else {
+                    string[] foodOptions = {"Vegatables", "Nuts and seeds", "Meat", "Seafood", "Beans"};
+                    string category = promptOptions("Choose a category of food!", foodOptions);
 
-            string[] options = {"Meal", "Food"};
-            string mealOrFood = promptOptions("Would you like a meal or a specific food?", options);
-
-            if (mealOrFood == "Meal") {
-                Console.WriteLine($"{Environment.NewLine}{randomItem(food.Meals)}");
-            } else {
-                string[] foodOptions = {"Vegatables", "Nuts and seeds", "Meat", "Seafood", "Beans"};
-                string category = promptOptions("Choose a category of food!", foodOptions);
-
-                if (category == "Nuts and seeds") {
-                    category = "Nuts_and_seeds";
-                }
-
-                Console.WriteLine($"{Environment.NewLine}{randomItem(food.Categories[category])}");
-
-                if (category == "Vegatables") {
-                    Console.WriteLine($"{Environment.NewLine}Heads-up! Some dark, leafy greens contain oxalates, which inhibit iron absorption. Try to incorporate other sources of iron as well!");
-                    Console.WriteLine("Another way to counteract this effect is to consume Vitamin C, which will increase iron absorption.");
-                    //Console.WriteLine("Would you like a recommendation for a fruit that contains Vitamin C?");
-
-                    string[] yesNo = {"Yes", "No"};
-                    string fruit = promptOptions("Would you like a recommendation for a fruit that contains Vitamin C?", yesNo);
-                    //string[] jsonFruits = 
-                    if (fruit == "Yes") {
-                        Console.WriteLine($"{Environment.NewLine}{randomItem(food.Categories["Fruits"])}");
-                    } else {
-
+                    if (category == "Nuts and seeds") {
+                        category = "Nuts_and_seeds";
                     }
-                    
+
+                    Console.WriteLine($"{Environment.NewLine}{randomItem(food.Categories[category])}");
+
+                    if (category == "Vegatables") {
+                        Console.WriteLine($"{Environment.NewLine}Heads-up! Some dark, leafy greens contain oxalates, which inhibit iron absorption. Try to incorporate other sources of iron as well!");
+                        Console.WriteLine("Another way to counteract this effect is to consume Vitamin C, which will increase iron absorption.");
+                        //Console.WriteLine("Would you like a recommendation for a fruit that contains Vitamin C?");
+
+                        string[] yesNo = {"Yes", "No"};
+                        string fruit = promptOptions("Would you like a recommendation for a fruit that contains Vitamin C?", yesNo);
+                        //string[] jsonFruits = 
+                        if (fruit == "Yes") {
+                            Console.WriteLine($"{Environment.NewLine}{randomItem(food.Categories["Fruits"])}");
+                        }
+                    }
+
+                    if (category == "Meat") {
+                        Console.WriteLine($"{Environment.NewLine}Great choice! Meat contains heme iron, which is much easier to absorb than its counterpart, non-heme iron!");
+                        Console.WriteLine("Keep in mind, red meat has the highest amount of iron, while poultry and chicken have lower amounts.");
+                    }
+
+                    if (category == "Beans") {
+                        Console.WriteLine($"{Environment.NewLine}Heads-up! Beans contain phylates, which can inhibit iron absorption! Be sure to include other sources of iron in your diet!");
+                    }
                 }
 
-                if (category == "Meat") {
-                    Console.WriteLine($"{Environment.NewLine}Great choice! Meat contains heme iron, which is much easier to absorb than its counterpart, non-heme iron!");
-                    Console.WriteLine("Keep in mind, red meat has the highest amount of iron, while poultry and chicken have lower amounts.");
-                }
+                //confirmEnd();
 
-                if (category == "Beans") {
-                    Console.WriteLine($"{Environment.NewLine}Heads-up! Beans contain phylates, which can inhibit iron absorption! Be sure to include other sources of iron in your diet!");
+                // give option to continue or end program
+                string[] endOptions = {"Look for more iron", "End da program"};
+                string choice = promptOptions("What would you like to do next?", endOptions);
+                if (choice == "End da program") {
+                    flag = false;
                 }
             }
+            
         }
 
         static void welcomeMsg()    
@@ -71,6 +83,10 @@ namespace JanID
             Console.WriteLine($"{Environment.NewLine}HEWWO my tubby cutie!");
             Console.WriteLine($"{Environment.NewLine}I built this to hopefully help you find yummy foods that are high in iron!");
             Console.WriteLine($"{Environment.NewLine}We will combat and prevail against your anemia.");
+
+            Console.WriteLine($"{Environment.NewLine}Press any key to begin!");
+            Console.ReadKey(true);
+
 
         }
 
@@ -101,6 +117,11 @@ namespace JanID
 
         static string randomItem(string[] items) {
             return items[new Random().Next(0, items.Length)];
+        }
+
+        static void confirmEnd() {
+            Console.WriteLine($"{Environment.NewLine}Press any key to end da program!");
+            Console.ReadKey(true);
         }
     }
 }
